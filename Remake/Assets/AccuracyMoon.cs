25a1746593eef505df5894eb53420722127d1672
Ayoze Manuel <hydekyle@hotmail.com>
2018-04-24 03:28:02 +0100

    Accuracy minigame

---
+++ Remake/Assets/AccuracyMoon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccuracyMoon : MonoBehaviour {
+
+    RectTransform t_pointer;
+    public byte difficulty = 2;
+    bool active = true;
+    bool dir;
+
+    void Start()
+    {
+        t_pointer = transform.Find("Pointer").GetComponent<RectTransform>();
+        t_pointer.localPosition = new Vector3(0, 300, 0);
+    }
+
+    void Update()
+    {
+        if (active)
+        {
+            if (dir)
+            {
+                t_pointer.localPosition = Vector3.Lerp(t_pointer.localPosition, new Vector3(0, -400, 0), Time.deltaTime * difficulty);
+            }
+            else
+            {
+                t_pointer.localPosition = Vector3.Lerp(t_pointer.localPosition, new Vector3(0, 400, 0), Time.deltaTime * difficulty);
+            }
+            if (t_pointer.localPosition.y > 300) dir = !dir;
+            if (t_pointer.localPosition.y < -300) dir = !dir;
+        }
+    }
+
+    public void Clicked()
+    {
+        active = !active;
+    }
+}
